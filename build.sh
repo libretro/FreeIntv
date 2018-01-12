a7fa51451867a554026955f8fcd2dfdbf3c0171b
markwkidd <mark@stardart.net>
2018-01-12 18:40:12 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-gcc -shared -fPIC -L src/ freeintv_libretro.c intv.c memory.c cp1610.c stic.c psg.c controller.c cart.c osd.c -lm -lc -lgcc -o freeintv_libretro.so
+cd src
+gcc -shared -fPIC freeintv_libretro.c intv.c memory.c cp1610.c stic.c psg.c controller.c cart.c osd.c -lm -lc -lgcc -o ../freeintv_libretro.so
+cd ..
