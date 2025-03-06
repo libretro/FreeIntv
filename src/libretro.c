81604511a86710b04873248098cac2d55061ad9f
Mike Sheehan <msheehan79@yahoo.com>
2025-03-06 00:29:19 -0500

    Add commit hash to the version string

--- src/libretro.c
+++ src/libretro.c
@@ -442,7 +442,10 @@ void retro_get_system_info(struct retro_system_info *info)
 {
 	memset(info, 0, sizeof(*info));
 	info->library_name = "FreeIntv";
-	info->library_version = "1.2";
+#ifndef GIT_VERSION
+#define GIT_VERSION ""
+#endif
+	info->library_version = "1.2 " GIT_VERSION;
 	info->valid_extensions = "int|bin|rom";
 	info->need_fullpath = true;
 }
